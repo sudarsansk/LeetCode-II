b4a17c9f096f720454e289263ed4ec99c6f10875
Sudarsan Kumaar S J <sudarsansk14@gmail.com>
2025-12-18 07:49:35 +0530

    Time: 23 ms (72.17%) | Memory: 90.3 MB (36.20%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // Map<Integer, Integer> map = new HashMap<>();
-        // for(int i=0; i < nums.length; i++){
-        //     if(map.containsKey(nums[i])){
-        //         if(i - map.get(nums[i]) <= k){
-        //             return true;
-        //         }
-        //     }
-        //     map.put(nums[i], i);
-        // }
-        // return false;
-
-        //Using HashSet
-        Set result = new HashSet();
-        for(int i=0; i<nums.length; i++){
-            if(result.contains(nums[i])) return true;
-            result.add(nums[i]);
-            if(result.size() > k){
-                result.remove(nums[i - k]);
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                if(i - map.get(nums[i]) <= k){
+                    return true;
+                }
             }
+            map.put(nums[i], i);
         }
         return false;
+
+        //Using HashSet
+        // Set result = new HashSet();
+        // for(int i=0; i<nums.length; i++){
+        //     if(result.contains(nums[i])) return true;
+        //     result.add(nums[i]);
+        //     if(result.size() > k){
+        //         result.remove(nums[i - k]);
+        //     }
+        // }
+        // return false;
     }
 }
